ec23d45b02f086133067ff0d57723e50593d10b6
debugger77 <devhr78@gmail.com>
2023-10-21 21:51:03 +0530

    first component created

---
+++ 01_basicreact/src/App.js
@@ -0,0 +1,13 @@
+
+import Pen from "./Pen";
+
+function App() {
+  return (
+    <>
+      <Pen />
+      <h1>Learn react</h1>
+    </>
+  )
+}
+
+export default App;
---
+++ 01_basicreact/src/Pen.js
@@ -0,0 +1,10 @@
+
+
+// if html is returned from the function then name it with .jsx extension in react
+function Pen() {
+    return(
+        <h2>Pen in react</h2>
+    )
+}
+
+export default Pen
